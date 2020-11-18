183668c7ef5c75173f0948a485f66ce7b6dcdad2
WendiF <wendi.fan@softwire.com>
2020-11-18 21:49:24 +0000

    Started Critter

--- src/App.java
+++ src/App.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Critter bob = new Critter();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What would you like to name your Critter?");
+        String name = scan.nextLine();
+        Critter bob = new Critter(name);
         Scanner scanner = new Scanner(System.in);
         while (bob.isAlive()) {
-            System.out.println("What would you like Critter to do?");
+            System.out.println("What would you like " + bob.getName() + " to do?");
             String lineRead = scanner.nextLine();
             if (lineRead.equalsIgnoreCase("eat")) {
                 bob.feed();
--- src/Critter.java
+++ src/Critter.java
@@ -2,11 +2,24 @@ public class Critter {
     private boolean isAlive;
     private int foodLevel;
     private int tiredness;
+    private String name;
 
-    public Critter() {
+    public Critter(String inputName) {
+        if (inputName.isEmpty())
+            name = "Critter";
+        else
+            name = inputName;
         isAlive = true;
         foodLevel = 5;
         tiredness = 0;
+    }
+
+    public void name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean isAlive() {
@@ -18,26 +31,26 @@ public class Critter {
     }
     
     public void sleep() {
-        System.out.println("Critter sleeps.");
+        System.out.println(name + " sleeps.");
         tiredness = 0;
         foodLevel -= 3;
         if (foodLevel <= 0) {
-            System.out.println("Critter starves to death.");
+            System.out.println(name + " starves to death.");
             die();
         }
     }
 
     public void feed() {
         if (isAlive) {
-            System.out.println("Critter eats.");
+            System.out.println(name + " eats.");
             foodLevel++;
             tiredness++;
             if (foodLevel > 10) {
-                System.out.println("Critter over ate.");
+                System.out.println(name + " over ate.");
                 die();
             }
             else if (tiredness > 5) {
-                System.out.println("Critter is sleepy from so much food.");
+                System.out.println(name + " is sleepy from so much food.");
                 sleep();
             }
         }
